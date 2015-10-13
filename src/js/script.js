4ddf42ada05a84ea64770e8b4cae46082a37402f
Юлия Антонова <antonova.julia@gmail.com>
2015-10-13 20:54:06 +0300

    тултип входа

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
 $(function() {
-
+	// toggle tooltip
+  $(".js-tooltip-toggle").on("click", function(event){
+    event.preventDefault();
+    $(this).parents(".tooltip-container").toggleClass("active");
+  })
 });
