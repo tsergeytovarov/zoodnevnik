81744b7a811bf7eb478347a82655a65f65f706ca
Юлия Антонова <antonova.julia@gmail.com>
2015-10-13 21:41:57 +0300

    поле поиска в хлебных крошках

--- src/js/script.js
+++ src/js/script.js
@@ -4,4 +4,16 @@ $(function() {
     event.preventDefault();
     $(this).parents(".tooltip-container").toggleClass("active");
   })
+
+  // открытие поля поиска в хлебных крошках
+  $(".js-search-open").on("click", function(event){
+    event.preventDefault();
+    $(".breadcrumbs-navigation__left").toggleClass("opened");
+  })
+
+  // закрытие поля поиска в хлебных крошках
+  $(".js-search-close").on("click", function(event){
+    event.preventDefault();
+    $(".breadcrumbs-navigation__left").removeClass("opened");
+  })
 });
